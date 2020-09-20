62a3d1794584e65f38d61ed34f4f2c6fd854c22d
Alexandr Siryj <theobroma222@gmail.com>
2020-09-20 06:06:50 +0300

    initial

---
+++ api/server.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import Cors from 'cors';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import passport from 'passport';
+
+const app = express();
+
+const API_PORT = process.env.API_PORT || 3000;
+
+const swaggerDefinition = {
+  info: {
+    title: 'MySQL Registration Swagger API',
+    version: '1.0.0',
+    description: 'Endpoints to test the user registration routes',
+  },
+  host: 'localhost:3003',
+  basePath: '/',
+  securityDefinitions: {
+    bearerAuth: {
+      type: 'apiKey',
+      name: 'Authorization',
+      scheme: 'bearer',
+      in: 'header',
+    },
+  },
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+require('./config/passport');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(Cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(passport.initialize());
+
+require('./routes/loginUser')(app);
+require('./routes/registerUser')(app);
+require('./routes/forgotPassword')(app);
+require('./routes/resetPassword')(app);
+require('./routes/updatePassword')(app);
+require('./routes/updatePasswordViaEmail')(app);
+require('./routes/findUsers')(app);
+require('./routes/deleteUser')(app);
+require('./routes/updateUser')(app);
+
+// eslint-disable-next-line no-console
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
+
+module.exports = app;
